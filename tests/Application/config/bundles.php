0c817a717d029133b17c6e81c08bc8d96a2e84c5
Mateusz Deszert-Klosowski <mateusz.deszert-klosowski@bitbag.pl>
2020-11-24 16:55:43 +0100

    [WIP] moved sitemap plugin to optional dependencies

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -58,6 +58,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
 
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
-    SitemapPlugin\SitemapPlugin::class => ['all' => true],
     BitBag\SyliusCmsPlugin\BitBagSyliusCmsPlugin::class => ['all' => true],
 ];
